49745660e5b4bacdca638be7eb07bec3abca862d
shandinan <shandinan@live.com>
2019-11-20 16:14:28 +0800

    sdn

--- QueuingSystem/MainForm.cs
+++ QueuingSystem/MainForm.cs
@@ -24,11 +24,27 @@ namespace QueuingSystem
         /// <param name="e"></param>
         private void btnTest_Click(object sender, EventArgs e)
         {
+            zh_show.Event_Log += _InfoLog;
             //Common.Util.RawPrint.SendStringToPrinter("", "ssss");
             // new Common.Util.ClsPrintLPT().PrintDataSet_test();
             zh_show.sendMsg2Screen("sssssssssssssssss");
 
-
+        }
+        /// <summary>
+        /// 普通日志
+        /// </summary>
+        /// <param name="info"></param>
+        private void _InfoLog(string info)
+        {
+            LogHelper.WriteLog(info);
+        }
+        /// <summary>
+        /// 错误日志
+        /// </summary>
+        /// <param name="err"></param>
+        private void _ErrLog(string err,Exception ex)
+        {
+            LogHelper.WriteLog(err, ex);
         }
     }
 }
